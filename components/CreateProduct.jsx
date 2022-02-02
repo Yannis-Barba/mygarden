6c1d3549be28ce349d1618b1bce9b702f6c1edd8
Yannis <yannis.barba.dev@gmail.com>
2022-02-02 14:52:09 +0100

    add a list of bad and good association with the product

--- components/CreateProduct.jsx
+++ components/CreateProduct.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import CloseIcon from '@mui/icons-material/Close';
+import axios from "axios";
 
 function CreateProduct({setShowCreateProduct}) {
 
@@ -12,19 +13,51 @@ function CreateProduct({setShowCreateProduct}) {
     const [recolteStart, setRecolteStart] = useState("janvier");
     const [recolteEnd, setRecolteEnd] = useState("janvier");
     const [imgUrl, setImgUrl] = useState("")
+    const [newGoodAssociation, setNewGoodAssociation] = useState("");
+    const [newBadAssociation, setNewBadAssociation] = useState("");
+    const [goodAssociation, setGoodAssociation] = useState([]);
+    const [badAssociation, setBadAssociation] = useState([]);
 
+    const [sendStatus, setSendStatus] = useState(false)
+
+    async function sendNewProduct(){
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_HOST_API_URL}/api/products`, {
+            name, 
+            genre, 
+            semisStart,
+            semisEnd, 
+            plantationStart, 
+            plantationEnd, 
+            recolteStart, 
+            recolteEnd, 
+            imgUrl, 
+            goodAssociation, 
+            badAssociation
+        })
+        setSendStatus(res.status === 201);
+    }
+
+    function addNewGoodAssociation(){
+        goodAssociation.push(newGoodAssociation);
+        setNewGoodAssociation("");
+    }
+
+    function addNewBadAssociation(){
+        badAssociation.push(newBadAssociation);
+        setNewBadAssociation("");
+    }
 
     const months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"]
     return (
-        <div className="absolute bg-gray-500/50 h-full w-full p-10 flex flex-col justify-center items-center">
-            <CloseIcon sx={{color: "#FFFFF4", fontSize: 50}} className="cursor-pointer" onClick={() => setShowCreateProduct(false)}/>
+        <div className="absolute bg-third h-fit w-full px-4 py-8 flex flex-col justify-center items-center">
+            <CloseIcon sx={{color: "#00477A", fontSize: 50}} className="absolute right-2 top-2 cursor-pointer" onClick={() => setShowCreateProduct(false)}/>
             <form className="bg-third py-4 px-2 flex flex-col gap-4 rounded-xl">
                 <h1 className="text-secondary text-4xl w-full text-center mb-4">Ajouter un produit</h1>
                 <label htmlFor="name" className="text-fourth/50 flex gap-4"> Name
-                    <input id="name"type="text" className="rounded-xl border-2 bg-transparent" value={name} onChange={(e) => setName(e.target.value)}></input>
+                    <input id="name"type="text" className="rounded-xl border-2 bg-transparent w-2/3" value={name} onChange={(e) => setName(e.target.value)}></input>
                 </label>
                 <label htmlFor="genre" className="text-fourth/50 flex gap-4"> Genre
-                    <input id="genre"type="text" className="rounded-xl border-2 bg-transparent" value={genre} onChange={(e) => setGenre(e.target.value)}></input>
+                    <input id="genre"type="text" className="rounded-xl border-2 bg-transparent w-2/3" value={genre} onChange={(e) => setGenre(e.target.value)}></input>
                 </label>
                 <h3>Période de semis : </h3>
                 <label htmlFor="semisStart" className="text-fourth/50 flex gap-4" > Début
@@ -86,8 +119,45 @@ function CreateProduct({setShowCreateProduct}) {
                 <label htmlFor="imgUrl" className="text-fourth/50 flex gap-4"> Image
                     <input id="imgUrl" type="text" className="rounded-xl border-2 bg-transparent" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)}></input>
                 </label>
+                <h3>Association(s) : </h3>
+                <label htmlFor="goodAssociation" className="text-fourth/50 flex gap-4 items-center"> Bonne(s)
+                    <input id="goodAssociation" type="text" className="rounded-xl border-2 bg-transparent w-2/3" value={newGoodAssociation} onChange={(e) => setNewGoodAssociation(e.target.value)}></input>
+                    <div className="bg-secondary/80 w-fit p-2 rounded-xl text-third font-medium cursor-pointer" onClick={() => addNewGoodAssociation()}>Ok</div>
+                </label>
+                {goodAssociation.length !== 0 && (
+                    <ul className="flex flex-wrap gap-4">
+                        {goodAssociation.map((product, index) => {
+                            return (
+                                <li key={index} className="bg-green-500/60 p-2 rounded-xl flex gap-2 items-center">
+                                    <h4>{product}</h4>
+                                    <CloseIcon sx={{color: "#FFFFF4", fontSize: 20}} className="cursor-pointer" onClick={() => {
+                                        setGoodAssociation(() => goodAssociation.filter((val) => val !== product))
+                                    }}/>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    )}
+                <label htmlFor="badAssociation" className="text-fourth/50 flex gap-4 items-center"> Mauvaise(s)
+                    <input id="badAssociation" type="text" className="rounded-xl border-2 bg-transparent w-2/3" value={newBadAssociation} onChange={(e) => setNewBadAssociation(e.target.value)}></input>
+                    <div className="bg-secondary/80 w-fit p-2 rounded-xl text-third font-medium cursor-pointer" onClick={() => addNewBadAssociation()}>Ok</div>
+                </label>
+                {goodAssociation.length !== 0 && (
+                    <ul className="flex flex-wrap gap-4">
+                        {badAssociation.map((product, index) => {
+                            return (
+                                <li key={index} className="bg-red-500/60 p-2 rounded-xl flex gap-2 items-center">
+                                    <h4>{product}</h4>
+                                    <CloseIcon sx={{color: "#FFFFF4", fontSize: 20}} className="cursor-pointer" onClick={() => {
+                                        setBadAssociation(() => badAssociation.filter((val) => val !== product))
+                                    }}/>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    )}
                 <div className="w-full flex justify-center">
-                    <div className="bg-secondary/80 w-fit p-2 rounded-xl text-third font-medium">Valider</div>
+                    <div className="bg-secondary/80 w-fit p-2 rounded-xl text-third font-medium" onClick={() => sendNewProduct()}>Valider</div>
                 </div>
             </form>
             
